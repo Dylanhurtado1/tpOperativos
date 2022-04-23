9b03c1e2c7590e90a0e3a18fff3d886f8b90c4d7
Dylanhurtado1 <dhurtado@frba.utn.edu.ar>
2022-04-23 18:58:25 -0300

    Actualizo planificador mediano plazo

--- kernel/src/planificador.h
+++ kernel/src/planificador.h
@@ -35,5 +35,8 @@ void admitir_proceso();
 void iniciar_cola_new();
 void eliminar_cola_new();
 
+// Planificador de mediano plazo
+void agregar_proceso_a_suspended_blocked(t_pcb *proceso);
+void iniciar_cola_suspended_blocked();
 
 #endif /* PLANIFICADOR_H_ */
--- kernel/src/planificador_mediano_plazo.c
+++ kernel/src/planificador_mediano_plazo.c
@@ -5,3 +5,12 @@
 t_queue *cola_suspended_blocked;
 t_queue *cola_suspended_ready;
 
+void agregar_proceso_a_suspended_blocked(t_pcb *proceso) {
+	queue_push(cola_suspended_blocked, proceso);
+}
+
+void iniciar_cola_suspended_blocked() {
+	cola_suspended_blocked = queue_create();
+}
+
+
