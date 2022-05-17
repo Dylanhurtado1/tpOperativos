f5df3639b31c0289a20f5b3b184322506efaa3d3
comejia <cmejia@frba.utn.edu.ar>
2022-05-17 02:01:27 -0300

    Clean kernel

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -14,15 +14,12 @@ int main(void) {
 
 	int socket_kernel = iniciar_modulo_servidor(kernel_config->ip_kernel, kernel_config->puerto_escucha, kernel_logger);
 
-	//inicio_estructuras_planificacion();
-
-    //inicio_planificacion();
 	iniciar_planificador();
 
 	log_info(kernel_logger, "Esperando conexion de consolas");
 	if(atender_clientes(socket_kernel, procesar_datos_consola) == WAIT_CLIENT_ERROR) {
 		log_error(kernel_logger, "Error al escuchar clientes... Finalizando servidor");
-		}
+	}
 	log_destroy(kernel_logger);
 	kernel_eliminar_configuracion(kernel_config);
 	cerrar_conexion(socket_kernel);
--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -1,10 +1,3 @@
-/*
- * kernel.h
- *
- *  Created on: 19 abr. 2022
- *      Author: utnso
- */
-
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
@@ -20,41 +13,6 @@
 #define PATH_KERNEL_CONFIG "/home/utnso/tp-2022-1c-lo-importante-es-aprobar/kernel/kernel.config"
 
 void iniciar_planificador();
-
-//KERNEL
-
-void iniciar_colas_de_planificacion();
-void inicio_estructuras_planificacion();
-void inicio_planificacion();
-
-
-//HILOS
-
-void iniciar_hilo_cola_new();
-void iniciar_hilo_cola_ready();
-void iniciar_hilo_cola_blocked();
-void iniciar_hilo_cola_suspended_blocked();
-void iniciar_hilo_cola_suspended_ready();
-
-
-//FUNCIONES PARA PLANIFICAR CADA COLA
-
-void planificar_cola_ready();
-void planificar_cola_block();
-void planificar_cola_suspended_blocked();
-void planificar_cola_suspended_ready();
-void planificar_cola_exec();
-
-
-//SEMAFOROS
-
-//pthread_mutex_t planificador_mutex_new;
-pthread_mutex_t planificador_mutex_ready;
-pthread_mutex_t planificador_mutex_exec;
-pthread_mutex_t planificador_mutex_blocked;
-pthread_mutex_t planificador_mutex_suspended_blocked;
-pthread_mutex_t planificador_mutex_suspended_ready;
-//pthread_mutex_t planificador_mutex_fin;
 
 
 #endif /* KERNEL_H_ */
