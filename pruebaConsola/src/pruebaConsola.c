b127df70d3ee570b6e8e323b2e1e4a7ce48500e2
Dylanhurtado1 <dhurtado@frba.utn.edu.ar>
2022-06-02 19:05:21 -0300

    Subiendo todo

---
+++ pruebaConsola/src/pruebaConsola.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : pruebaConsola.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	printf("Que ondaaa");
+
+	return EXIT_SUCCESS;
+}
---
+++ pruebaKernel/src/pruebaKermel.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : pruebaKermel.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("Recibo algo de Consola");
+	return EXIT_SUCCESS;
+}
