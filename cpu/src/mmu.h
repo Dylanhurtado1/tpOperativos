6f9ba7cfcb694c9dee6aa0b34d9e4e109f76b1e3
comejia <cmejia@frba.utn.edu.ar>
2022-06-11 02:38:02 -0300

    Mejora en metodos y uso de TLB para traducir dir logica

--- cpu/src/mmu.h
+++ cpu/src/mmu.h
@@ -2,6 +2,7 @@
 #define MMU_H_
 
 #include "cpu_global.h"
+#include "tlb.h"
 
 typedef struct {
 	uint32_t numero_pagina;
@@ -14,10 +15,7 @@ uint32_t traducir_direccion_logica(uint32_t tabla_primer_nivel, uint32_t direcci
 uint32_t leer_memoria(uint32_t direccion_fisica);
 void escribir_memoria(uint32_t direccion_fisica, uint32_t valor);
 t_traducciones parsear_direccion_logica(uint32_t direcion_logica);
-uint32_t obtener_tabla_segundo_nivel(int socket_memoria, uint32_t tabla_primer_nivel, uint32_t entrada_tabla_nivel_1);
-uint32_t obtener_numero_de_marco(int socket_memoria, uint32_t tabla_segundo_nivel, uint32_t entrada_tabla_nivel_2);
-uint32_t obtener_valor_de_memoria(int socket_memoria, uint32_t direccion_fisica);
-void enviar_valor_a_memoria(int socket_memoria, uint32_t direccion_fisica, uint32_t valor);
+uint32_t acceder_tablas_en_memoria(uint32_t direccion_tabla, uint32_t entrada_tabla, t_protocolo protocolo);
 
 
 #endif /* MMU_H_ */
