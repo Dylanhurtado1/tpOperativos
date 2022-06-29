6bb55ffc230e9344a1b80583c4c119ff4547a5e2
comejia <cmejia@frba.utn.edu.ar>
2022-06-29 15:41:07 -0300

    Retardo memoria

--- memoria/src/gestionar_conexiones.c
+++ memoria/src/gestionar_conexiones.c
@@ -1,5 +1,7 @@
 #include "gestionar_conexiones.h"
 
+static void retardo_respuesta(uint32_t tiempo);
+
 
 void procesar_conexiones(t_cliente *datos_cliente) {
 	t_pcb *pcb;
@@ -15,7 +17,7 @@ void procesar_conexiones(t_cliente *datos_cliente) {
 			log_info(memoria_logger, "Inicializando estructuras de PID[%d]...", pcb->id);
 			swap_crear_archivo(pcb->id, pcb->tamanio_proceso);
 			uint32_t tabla_primer_nivel = crear_tablas_de_paginacion(pcb->id);
-			enviar_direccion_tabla_de_pagina(datos_cliente->socket, tabla_primer_nivel);
+			enviar_direccion_tabla_primer_nivel(datos_cliente->socket, tabla_primer_nivel);
 
 			eliminar_pcb(pcb);
 			break;
@@ -50,7 +52,7 @@ void procesar_conexiones(t_cliente *datos_cliente) {
 			log_info(memoria_logger, "Enviando tabla segundo nivel...");
 			tabla = deserializar_tabla_de_acceso(paquete);
 			uint32_t tabla_segundo_nivel = get_tabla_segundo_nivel(tabla->direccion, tabla->entrada);
-			enviar_direccion_tabla_de_pagina(datos_cliente->socket, tabla_segundo_nivel);
+			enviar_direccion_tabla_segundo_nivel(datos_cliente->socket, tabla_segundo_nivel);
 
 			eliminar_tabla_de_acceso(tabla);
 			break;
@@ -82,6 +84,7 @@ void procesar_conexiones(t_cliente *datos_cliente) {
 			log_info(memoria_logger, "Valor = %d", valor_a_escribir);
 
 			escribir_memoria_principal(direccion_fisica, valor_a_escribir);
+			// TODO: ver si se escribe en memoria correctamente o no y enviar respuesta acorde a CPU
 			actualizar_pagina_modificada(marco_modificado(direccion_fisica));
 
 			list_destroy_and_destroy_elements(datos, free);
@@ -93,26 +96,38 @@ void procesar_conexiones(t_cliente *datos_cliente) {
 	eliminar_paquete(paquete);
 }
 
-void enviar_direccion_tabla_de_pagina(int socket_fd, uint32_t direccion) {
+void enviar_direccion_tabla_primer_nivel(int socket_fd, uint32_t direccion) {
 	enviar_datos(socket_fd, &direccion, sizeof(uint32_t));
-}
-
-void enviar_numero_marco_de_pagina(int socket_fd, uint32_t numero) {
-	enviar_datos(socket_fd, &numero, sizeof(uint32_t));
-}
-
-void enviar_valor_leido_de_memoria(int socket_fd, uint32_t valor) {
-	enviar_datos(socket_fd, &valor, sizeof(uint32_t));
-}
-
-void enviar_estructura_traductora(int socket_fd, t_traductor *traductor) {
-	t_paquete *paquete = serializar_traductor(traductor, HANDSHAKE_INICIAL);
-	enviar_paquete(paquete, socket_fd);
-	eliminar_paquete(paquete);
 }
 
 void informar_estado_proceso(int socket_fd, t_protocolo protocolo) {
 	enviar_datos(socket_fd, &protocolo, sizeof(t_protocolo));
 }
 
+void enviar_estructura_traductora(int socket_fd, t_traductor *traductor) {
+	t_paquete *paquete = serializar_traductor(traductor, HANDSHAKE_INICIAL);
+	retardo_respuesta(memoria_config->retardo_memoria);
+	enviar_paquete(paquete, socket_fd);
+	eliminar_paquete(paquete);
+}
+
+void enviar_direccion_tabla_segundo_nivel(int socket_fd, uint32_t direccion) {
+	retardo_respuesta(memoria_config->retardo_memoria);
+	enviar_datos(socket_fd, &direccion, sizeof(uint32_t));
+}
+
+void enviar_numero_marco_de_pagina(int socket_fd, uint32_t numero) {
+	retardo_respuesta(memoria_config->retardo_memoria);
+	enviar_datos(socket_fd, &numero, sizeof(uint32_t));
+}
+
+void enviar_valor_leido_de_memoria(int socket_fd, uint32_t valor) {
+	retardo_respuesta(memoria_config->retardo_memoria);
+	enviar_datos(socket_fd, &valor, sizeof(uint32_t));
+}
+
+
+static void retardo_respuesta(uint32_t tiempo) {
+	usleep(tiempo * 1000);
+}
 
--- memoria/src/gestionar_conexiones.h
+++ memoria/src/gestionar_conexiones.h
@@ -7,11 +7,12 @@
 #include "swap.h"
 
 void procesar_conexiones(t_cliente *datos_cliente);
-void enviar_direccion_tabla_de_pagina(int socket_fd, uint32_t direccion);
+void enviar_direccion_tabla_primer_nivel(int socket_fd, uint32_t direccion);
+void informar_estado_proceso(int socket_fd, t_protocolo protocolo);
+void enviar_estructura_traductora(int socket_fd, t_traductor *traductor);
+void enviar_direccion_tabla_segundo_nivel(int socket_fd, uint32_t direccion);
 void enviar_numero_marco_de_pagina(int socket_fd, uint32_t numero);
 void enviar_valor_leido_de_memoria(int socket_fd, uint32_t valor);
-void enviar_estructura_traductora(int socket_fd, t_traductor *traductor);
-void informar_estado_proceso(int socket_fd, t_protocolo protocolo);
 
 
 #endif /* GESTIONAR_CONEXIONES_H_ */
