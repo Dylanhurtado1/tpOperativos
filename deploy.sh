6d7a1df3b0df66984c37bd05549e5e24a514979d
comejia <cmejia@frba.utn.edu.ar>
2022-07-11 21:21:50 -0300

    Mejora archivo deploy

--- deploy.sh
+++ deploy.sh
@@ -3,60 +3,46 @@
 PROJECT_PATH=/home/utnso/tp-2022-1c-lo-importante-es-aprobar
 TESTING_PATH=/home/utnso/kiss-pruebas
 SWAP_PATH=/home/utnso/swap
+COMMON_PATH=/home/utnso/swap/so-commons-library
 
 if [ "$1" == "" ]; then
-	echo "Error de ejecucion: ./deploy.sh <proceso> [path_instrucciones] [tam_memoria]"
+	echo "Error de ejecucion: ./deploy.sh <install | uninstall>"
 	exit(1)
 fi
 
-proceso=$1
 
-echo -e "\n\nInstalling commons libraries...\n\n"
+if [ "$1" == "install" ]; then
 
-cd $HOME
-
-COMMONS="so-commons-library"
-
-rm -rf $COMMONS
-git clone "https://github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
-cd $COMMONS
-sudo make uninstall
-sudo make install
+	echo -e "\n\nInstalling commons libraries...\n\n"
+	cd $HOME
+	COMMONS="so-commons-library"
+	rm -rf $COMMONS
+	git clone "https://github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
+	cd $COMMONS
+	sudo make uninstall
+	sudo make install
 
 
-echo -e "\n\nInstalling commons libraries... DONE\n\n"
-
-echo -e "\n\n **** Descargando Pruebas... ****\n\n"
-cd $HOME
-git clone "https://github.com/sisoputnfrba/kiss-pruebas"
-
-echo -e "\n\n **** Descargando Pruebas... DONE ****\n\n"
+	echo -e "\n\n **** Descargando Pruebas... ****\n\n"
+	cd $HOME
+	git clone "https://github.com/sisoputnfrba/kiss-pruebas"
 
 
-echo -e "\n\n ***** Ejecutando modulo: $proceso *****\n"
+	echo -e "\n\n ***** Se instalo todo. Ejecutar modulos *****\n"
+	mkdir -p $SWAP_PATH
+	cd $PROJECT_PATH
 
-mkdir -p $SWAP_PATH
-cd $PROJECT_PATH
+else
+	echo -e "\n\n **** Desinstalando todo... ****\n\n"
+	cd $COMMON_PATH
+	sudo make uninstall
+	cd $HOME
+	rm -rf $COMMON_PATH
 
-do
-    case $proceso in
-        "consola")
-		PATH_I=${TESTING_PATH}/$2
-		TAMANIO=$3
-		make consola $PATH_I $TAMANIO
-        	;;
-	"kernel")
-		make kernel
-        	;;
-        "cpu")
-		make cpu
-        	;;
-	"memoria")
-		make memoria
-        *)
-		echo "Proceso $proceso invalido."
-		exit(1)
-        	;;
-    esac
-done
+	rm -rf $TESTING_PATH
+	rm -rf $SWAP_PATH
+	
+	rm -rf $PROJECT_PATH
+fi	
+
 
